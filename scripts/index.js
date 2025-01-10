998af72331cc6f3f4912117c59ebd94a85202fe4
Lesan Abbas <63160640+lesanabbas@users.noreply.github.com>
2025-01-10 20:26:48 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -158,3 +158,17 @@ $(window).on("load", () => {
     .setTween(sideNavTL)
     .addTo(controller);
 });
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
